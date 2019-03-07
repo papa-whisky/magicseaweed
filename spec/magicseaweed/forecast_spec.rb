d364a2b6caeaa7a26e77e9e7c49b78fc9b8552c7
Paul Williams <paul@pdwilliams.net>
2019-03-07 22:23:04 +1100

    Forecast specs

--- spec/magicseaweed/forecast_spec.rb
+++ spec/magicseaweed/forecast_spec.rb
@@ -3,7 +3,27 @@
 require 'support/webmock/fixtures'
 
 RSpec.describe Magicseaweed::Forecast do
+  include WebMock::Fixtures
+
+  let(:forecast) { described_class.new(api_key: 'api_key', spot_id: 'spot_id') }
+
   describe '#to_s' do
-    it 'returns a forecast'
+    subject(:to_s) { forecast.to_s }
+
+    let(:api_client) { instance_double('Magicseaweed::APIClient') }
+
+    before do
+      allow(Magicseaweed::APIClient).to receive(:new)
+        .with(api_key: 'api_key').and_return(api_client)
+
+      allow(api_client).to receive(:get_forecast)
+        .with(spot_id: 'spot_id', fields: 'localTimestamp,solidRating')
+        .and_return(JSON.parse(fixture_response_body(:success)))
+    end
+
+    it 'returns a forecast for the next five days' do
+      expect(to_s).to eq "Spot: spot_id\nForecast:\n" \
+        'Tue: , Wed: **, Thu: *, Fri: *, Sat: **'
+    end
   end
 end
